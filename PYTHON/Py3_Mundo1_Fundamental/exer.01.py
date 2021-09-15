2d7035970b4536b4fd498116f2e29c8b36b5ae3c
Marcio Alessandro Dos Santos <marciobroficial@outlook.com>
2021-09-14 23:08:43 -0300

    Create exer.01.py

---
+++ PYTHON/Py3_Mundo1_Fundamental/exer.01.py
@@ -0,0 +1,22 @@
+# #1 faça um programa que escreva "Ola Mundo".
+# #-------------------------------------------#
+# print()
+# print('=-='*6)
+# print ("Olá Mundo !!!...")
+# print('=-='*6)
+# print()
+# #-------------------------------------------#
+
+# Desafio 01
+# Faça um programa Leia um nome de uma pessoa e que mostre uma mensagem de boas-vidas de acordo com o valor digitado.
+
+print()
+print('=-='*15)
+nome = input ('Qual é sue nome ? ')
+print ('Ola',nome,'Prazer em te conhecer!')
+
+nome = input('Digite seu nome: ')
+print('É um prazer te conhecer, {}!'.format(nome))
+
+print('=-='*15)
+print()
