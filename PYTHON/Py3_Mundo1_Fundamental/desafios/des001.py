ea97b1121319190c756d75e861e2be7609e9f89d
Marcio Alessandro Dos Santos <marciobroficial@outlook.com>
2021-09-14 23:17:07 -0300

    Primeiro exer em py

---
+++ PYTHON/Py3_Mundo1_Fundamental/desafios/des001.py
@@ -0,0 +1,13 @@
+# Desafio 01
+# Faça um programa Leia um nome de uma pessoa e que mostre uma mensagem de boas-vidas de acordo com o valor digitado.
+
+print()
+print('=-='*15)
+nome = input ('Qual é sue nome ? ')
+print ('Ola',nome,'Prazer em te conhecer!')
+
+nome = input('Digite seu nome: ')
+print('É um prazer te conhecer, {}!'.format(nome))
+
+print('=-='*15)
+print()
