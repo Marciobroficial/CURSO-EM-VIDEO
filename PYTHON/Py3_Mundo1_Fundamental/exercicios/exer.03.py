0c2f5477d1decece051d2c31e605de51d410ba59
Marcio Alessandro Dos Santos <marciobroficial@outlook.com>
2021-09-26 13:49:58 -0300

    ex03

---
+++ PYTHON/Py3_Mundo1_Fundamental/exercicios/exer.03.py
@@ -0,0 +1,8 @@
+# 3º Desafio.
+# Crie um programa que leia dois numeros e mostre a soma entre eles.
+#-------------------------------------------------------------------#
+
+n1 = int (input ('Digite um numero: '))
+n2 = int (input ('Digitw mais um numero: '))
+s = n1 + n2
+print ('A soma entra {} e {} dois numeros é: {}'.format(n1,n2,s))
